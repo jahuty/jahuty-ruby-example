9da7ea4dcfdbb0d9974d2e062b7b523a312fc839
Jack Clayton <clayjs0@gmail.com>
2021-04-19 20:28:09 -0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :examples, only: [:index]
+  get 'examples', to: 'examples#index'
+  scope 'examples', controller: :examples do
+    get 'snippet'
+    get 'snippet-with-params'
+    get 'snippet-from-cache'
+    get 'snippets'
+    get 'snippets-with-params'
+    get 'snippets-from-cache'
+  end
 end
